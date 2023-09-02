282a8a9f72193056402fc108df541bd10fd7fd17
Bill Rossi <bill@localhost.localdomain>
2023-09-02 17:08:34 -0400

    Suru and kuru

--- src/IkaVerb.ts
+++ src/IkaVerb.ts
@@ -16,22 +16,57 @@ export class IkaVerb {
     public exceptions?: IkaVerbExceptions
   ) {}
 
+  get isSuru(): boolean {
+	  return this.dictionaryForm.length >= 2 && 
+	    this.dictionaryForm[this.dictionaryForm.length - 2] === "す" &&
+	    this.dictionaryForm[this.dictionaryForm.length - 1] === "る"
+  }
+
+  get isKuru(): boolean {
+    return this.dictionaryForm.length >= 2 && 
+	    this.dictionaryForm[this.dictionaryForm.length - 2][0] === "来" &&
+	    this.dictionaryForm[this.dictionaryForm.length - 2][1] === "く" &&
+	    this.dictionaryForm[this.dictionaryForm.length - 1] === "る"
+  }
+
+  get suruStem(): IkaStr {
+    if (this.dictionaryForm.length == 2) return ["し"]
+    const start = this.dictionaryForm.slice(0, -2)
+    return start.concat(["し"])
+  }
+
+  get kuruStem(): IkaStr {
+    if (this.dictionaryForm.length == 2) return [["来", "き"]]
+    const start = this.dictionaryForm.slice(0, -2)
+    return start.concat([["来", "き"]])
+  }
+
   get lastChar(): string {
     return this.dictionaryForm.slice(-1)[0] as string
   }
 
-  get stem() {
+  get stem(): IkaStr {
     switch(this.verbType) {
       case IkaVerbType.Ichidan:
 	return this.dictionaryForm.slice(0, -1)
       case IkaVerbType.Godan:
         return this.dictionaryForm.slice(0, -1).concat([hiraganaChart[this.lastChar].i])
       case IkaVerbType.Irregular:
-	return ["c"]
+	if (this.isSuru)
+	    return this.suruStem
+	if (this.isKuru)
+	    return this.kuruStem
+
+      console.error(`Invalid verb ${this.dictionaryForm}`)
+      return []
     }
   }
 
-  get positivePresentLongForm(): IkaStr {
+  get presentAffirmativeLongForm(): IkaStr {
     return this.stem.concat(["ます"])
+  }
+
+  get presentNegativeLongForm(): IkaStr {
+    return this.stem.concat(["ません"])
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { ikaStringToHTMLString } from './IkaStr.js'
+import { ikaStringToHTMLString, ikaStringToKanji } from './IkaStr.js'
 import { IkaVerb, IkaVerbType } from './IkaVerb.js'
 
 const tabemasu = new IkaVerb(
   [["食", "た"], "べ", "る"],
   IkaVerbType.Ichidan,
-).positivePresentLongForm
+).presentAffirmativeLongForm
 const tabemasuDiv = document.createElement('span')
 tabemasuDiv.innerHTML = ikaStringToHTMLString(tabemasu)
 document.body.appendChild(tabemasuDiv)
@@ -12,7 +12,19 @@ document.body.appendChild(tabemasuDiv)
 const ikimasu = new IkaVerb(
   [["行", "い"], "く"],
   IkaVerbType.Godan,
-).positivePresentLongForm
+).presentAffirmativeLongForm
 const ikimasuDiv = document.createElement('span')
 ikimasuDiv.innerHTML = ikaStringToHTMLString(ikimasu)
 document.body.appendChild(ikimasuDiv)
+
+const suru = new IkaVerb(["す","る"], IkaVerbType.Irregular)
+console.dir(suru.presentAffirmativeLongForm)
+console.dir(suru.presentNegativeLongForm)
+
+const kuru = new IkaVerb([["来", "く"],"る"], IkaVerbType.Irregular)
+console.dir(kuru.presentAffirmativeLongForm)
+console.dir(kuru.presentNegativeLongForm)
+
+const study = new IkaVerb([["勉","べん"], ["強","きょう"], "す","る"], IkaVerbType.Irregular)
+console.dir(ikaStringToKanji(study.presentAffirmativeLongForm))
+console.dir(ikaStringToKanji(study.presentNegativeLongForm))
